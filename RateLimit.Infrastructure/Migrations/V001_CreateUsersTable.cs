3febcf574bc6099d4e03818b06e3f32b681e90f2
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-22 14:03:00 -0300

    feat: Cria migração responsável por criar tabela de usuários

---
+++ RateLimit.Infrastructure/Migrations/V001_CreateUsersTable.cs
@@ -0,0 +1,23 @@
+using FluentMigrator;
+
+namespace RateLimit.Infrastructure.Migrations;
+
+[Migration(1)]
+public class V001_CreateUsersTable : Migration
+{
+    private const string TableName = "TB_USERS";
+    
+    public override void Up()
+    {
+        Create.Table(TableName)
+            .WithColumn("ID").AsInt32().PrimaryKey().Identity()
+            .WithColumn("EMAIL").AsString(100).NotNullable()
+            .WithColumn("PASSWORD").AsString(360).NotNullable()
+            .WithColumn("PLAN").AsInt32().NotNullable().WithDefaultValue(0);
+    }
+
+    public override void Down()
+    {
+        Delete.Table(TableName);
+    }
+}
