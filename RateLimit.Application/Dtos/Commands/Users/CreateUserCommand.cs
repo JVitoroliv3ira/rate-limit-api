f8c6fda53358394f11d398bf0c8a64567ec54d6e
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-22 17:53:44 -0300

    feat: Adiciona anotações para validação

--- RateLimit.Application/Dtos/Commands/Users/CreateUserCommand.cs
+++ RateLimit.Application/Dtos/Commands/Users/CreateUserCommand.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RateLimit.Application.Dtos.Commands.Users;
 
 public record CreateUserCommand(
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email is invalid")]
     string Email,
+
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
     string Password
 );
