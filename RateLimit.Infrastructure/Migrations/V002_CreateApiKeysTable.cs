d189a5344ff76fbed431971011aac7e348aabc66
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-22 14:11:31 -0300

    feat: Adiciona migração responsável por criar tabela de chaves de api

---
+++ RateLimit.Infrastructure/Migrations/V002_CreateApiKeysTable.cs
@@ -0,0 +1,31 @@
+using FluentMigrator;
+
+namespace RateLimit.Infrastructure.Migrations;
+
+[Migration(2)]
+public class V002_CreateApiKeysTable : Migration
+{
+    private const string TableName = "TB_API_KEYS";
+    private const string UsersTable = "TB_USERS";
+    private const string ForeignKeyName = "FK_API_KEYS_USERS";
+    
+    public override void Up()
+    {
+        Create.Table(TableName)
+            .WithColumn("ID").AsInt32().PrimaryKey().Identity()
+            .WithColumn("KEY").AsString(128).NotNullable()
+            .WithColumn("IS_ACTIVE").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("CREATED_AT").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime)
+            .WithColumn("USER_ID").AsInt32().NotNullable();
+
+        Create.ForeignKey(ForeignKeyName)
+            .FromTable(TableName).ForeignColumn("USER_ID")
+            .ToTable(UsersTable).PrimaryColumn("ID");
+    }
+
+    public override void Down()
+    {
+        Delete.ForeignKey(ForeignKeyName).OnTable(TableName);
+        Delete.Table(TableName);
+    }
+}
