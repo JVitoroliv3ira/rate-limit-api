56a0a0a755556e68b41e5d811fdc63081e704115
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-22 17:06:17 -0300

    fix: Ajusta métodos do repository

--- RateLimit.Infrastructure/Repositories/Repository.cs
+++ RateLimit.Infrastructure/Repositories/Repository.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using RateLimit.Application.Interfaces.Core;
 using RateLimit.Infrastructure.Persistence;
@@ -9,18 +10,38 @@ public class Repository<T>(
 ) : IRepository<T> where T : class
 {
     private readonly DbSet<T> _dbSet = dbContext.Set<T>();
-    
-    public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
 
-    public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+    public async Task<T?> GetByAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _dbSet.FirstOrDefaultAsync(predicate, cancellationToken);
+    }
+
+    public async Task<IList<T>> ListByAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _dbSet.Where(predicate).ToListAsync(cancellationToken);
+    }
+
+    public async Task DeleteByAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+    {
+        var entities = await _dbSet.Where(predicate).ToListAsync(cancellationToken);
+        _dbSet.RemoveRange(entities);
+    }
+
+    public async Task<bool> ExistByAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _dbSet.AnyAsync(predicate, cancellationToken);
+    }
+
+    public async Task AddAsync(T entity, CancellationToken cancellationToken)
+    {
+        await _dbSet.AddAsync(entity, cancellationToken);
+    }
 
     public Task UpdateAsync(T entity)
     {
         _dbSet.Update(entity);
         return Task.CompletedTask;
     }
-
-    public void Delete(T entity) => _dbSet.Remove(entity);
 
     public IQueryable<T> Queryable => _dbSet.AsQueryable();
 }
