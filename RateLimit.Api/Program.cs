9904e36e88d5212546d59c0110d8f7e04d96e43b
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-22 17:08:00 -0300

    feat: Configura injeção de dependencia

--- RateLimit.Api/Program.cs
+++ RateLimit.Api/Program.cs
@@ -1,7 +1,13 @@
 
 using Microsoft.EntityFrameworkCore;
+using RateLimit.Application.Interfaces.Core;
+using RateLimit.Application.Interfaces.Services;
+using RateLimit.Application.Interfaces.UseCases;
+using RateLimit.Application.UseCases.Users;
 using RateLimit.Infrastructure.MigrationRunner;
 using RateLimit.Infrastructure.Persistence;
+using RateLimit.Infrastructure.Repositories;
+using RateLimit.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +19,13 @@ var connectionString = builder.Configuration.GetConnectionString("Default") ?? t
 
 builder.Services.AddMigrationRunner(connectionString);
 builder.Services.AddDbContext<AppDbContext>(o => o.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddScoped<IPasswordHasher, BcryptPasswordHasher>();
+
+builder.Services.AddScoped<ICreateUserUseCase, CreateUserUseCase>();
 
 var app = builder.Build();
 
