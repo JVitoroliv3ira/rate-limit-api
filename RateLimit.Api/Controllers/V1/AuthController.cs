13edad2b3abb503906496dec79230c0bf085c78a
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-23 21:18:28 -0300

    fix: Renomeia Controller. feat: Adiciona endpoint responsável pela autenticação.

--- RateLimit.Api/Controllers/V1/AuthController.cs
+++ RateLimit.Api/Controllers/V1/AuthController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RateLimit.Application.Dtos.Commands.Auth;
 using RateLimit.Application.Dtos.Commands.Users;
 using RateLimit.Application.Interfaces.UseCases;
 
@@ -7,17 +8,18 @@ namespace RateLimit.Api.Controllers.V1;
 [Route("api/v1/[controller]")]
 [ApiController]
 public class AuthController(
-    ICreateUserUseCase createUserUseCase    
+    ICreateUserUseCase createUserUseCase,
+    IAuthenticateUserUseCase authenticateUserUseCase
 ) : ControllerBase
 {
-    [HttpPost]
-    public async Task<IActionResult> Create(
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(
         [FromBody] CreateUserCommand command,
         CancellationToken cancellationToken
     )
     {
         var result = await createUserUseCase.ExecuteAsync(command, cancellationToken);
-        
+
         return result.Match<IActionResult>(
             error => error.Code switch
             {
@@ -26,7 +28,27 @@ public class AuthController(
                 "unauthorized" => Unauthorized(error),
                 _ => StatusCode(500, error)
             },
-            success => CreatedAtAction(nameof(Create), new { id = success.Id }, success)
+            success => CreatedAtAction(nameof(Register), new { id = success.Id }, success)
+        );
+    }
+
+    [HttpPost("token")]
+    public async Task<IActionResult> Login(
+        [FromBody] AuthenticateUserCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await authenticateUserUseCase.ExecuteAsync(command, cancellationToken);
+
+        return result.Match<IActionResult>(
+            error => error.Code switch
+            {
+                "validation" => BadRequest(error),
+                "conflict" => Conflict(error),
+                "unauthorized" => Unauthorized(error),
+                _ => StatusCode(500, error)
+            },
+            Ok
         );
     }
 }
