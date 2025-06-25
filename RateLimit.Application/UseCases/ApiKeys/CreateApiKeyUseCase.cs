f483983851f0488db483e7e7fc1d51b0a0dc077f
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-24 21:11:11 -0300

    feat: Implementa use case

---
+++ RateLimit.Application/UseCases/ApiKeys/CreateApiKeyUseCase.cs
@@ -0,0 +1,48 @@
+using RateLimit.Application.Common;
+using RateLimit.Application.Dtos.Commands.ApiKeys;
+using RateLimit.Application.Dtos.Results.ApiKeys;
+using RateLimit.Application.Interfaces.Core;
+using RateLimit.Application.Interfaces.Services;
+using RateLimit.Application.Interfaces.UseCases;
+using RateLimit.Domain.Entities;
+
+namespace RateLimit.Application.UseCases.ApiKeys;
+
+public class CreateApiKeyUseCase(
+    IRepository<User> userRepository,
+    IRepository<ApiKey> apiKeyRepository,
+    IApiTokenService apiTokenService
+) : ICreateApiKeyUseCase
+{
+    public async Task<Either<ApiError, CreateApiKeyResult>> ExecuteAsync(
+        CreateApiKeyCommand command,
+        int userId,
+        CancellationToken cancellationToken)
+    {
+        var userExists = await userRepository.ExistByAsync(u => u.Id == userId, cancellationToken);
+        if (!userExists)
+        {
+            return ApiError.Validation("User not found.");
+        }
+
+        var (apiKey, hashedApiKey) = apiTokenService.Generate();
+
+        var key = new ApiKey
+        {
+            Name = command.Name,
+            Key = hashedApiKey,
+            CreatedAt = DateTime.UtcNow,
+            IsActive = true,
+            UserId = userId
+        };
+
+        await apiKeyRepository.AddAsync(key, cancellationToken);
+
+        var result = new CreateApiKeyResult(
+            Name: key.Name,
+            Key: apiKey
+        );
+
+        return result;
+    }
+}
