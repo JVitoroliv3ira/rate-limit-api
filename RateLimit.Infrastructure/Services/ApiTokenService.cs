1001b0942ae227f5aeca4fedce56a14109668a32
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-06-24 21:12:18 -0300

    feat: Implementa service

---
+++ RateLimit.Infrastructure/Services/ApiTokenService.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography;
+using System.Text;
+using RateLimit.Application.Interfaces.Services;
+
+namespace RateLimit.Infrastructure.Services;
+
+public class ApiTokenService : IApiTokenService
+{
+    public (string token, string hash) Generate()
+    {
+        var bytes = RandomNumberGenerator.GetBytes(32);
+        var token = Convert.ToHexString(bytes).ToLower();
+        var hash = Hash(token);
+        return (token, hash);
+    }
+
+    public bool Validate(string token, string hash)
+    {
+        if (string.IsNullOrWhiteSpace(token) || string.IsNullOrWhiteSpace(hash))
+            return false;
+
+        var tokenHash = Hash(token);
+
+        var a = Encoding.ASCII.GetBytes(tokenHash);
+        var b = Encoding.ASCII.GetBytes(hash.ToLower());
+
+        return CryptographicOperations.FixedTimeEquals(a, b);
+    }
+    
+    private static string Hash(string input)
+    {
+        var bytes = Encoding.UTF8.GetBytes(input);
+        var hash = SHA256.HashData(bytes);
+        return Convert.ToHexString(hash).ToLower();
+    }
+}
